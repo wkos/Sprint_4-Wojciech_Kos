e3b969fd8927a97a025aeea6855713ad8b3c0d0c
Wojciech Kos <obohp@wp.pl>
2024-08-21 11:37:50 +0200

    Program after feedback

--- tasklist.js
+++ tasklist.js
@@ -1,68 +1,65 @@
-const parent = document.getElementById("taskContainer");
-const taskList = document.createElement("ul");
-taskList.setAttribute("class", "taskList");
-parent.appendChild(taskList);
+const taskList = document.querySelector("ul");
 
-document.getElementById("addTask").addEventListener("click", createTask);
+// document.getElementById("addTask").addEventListener("click", createTask); zamiast tego lepiej to, co niżej.
+// Dobrą praktyką jest pobranie referencji do danego elementu, zapisanie jej do zmiennej, a następnie przekazanie listenera.
+
+const addTaskButton = document.getElementById("addTask");
+addTaskButton.addEventListener("click", createTask);
+const taskInput = document.getElementById("taskTitle");
+
+function createButton(
+  buttonClass,
+  textOnButton,
+  elementHTML,
+  handlingFunction
+) {
+  const newButton = document.createElement("button");
+  newButton.className = buttonClass;
+  newButton.textContent = textOnButton;
+  elementHTML.appendChild(newButton);
+  newButton.addEventListener("click", handlingFunction);
+}
 
 function createTask() {
-  taskInput = document.getElementById("taskTitle");
   if (taskInput.value !== "") {
     const newTask = document.createElement("li");
     newTask.textContent = taskInput.value;
     taskList.appendChild(newTask);
 
-    const newEditButton = document.createElement("button");
-    newEditButton.setAttribute("class", "button");
-    newEditButton.textContent = "Edytuj";
-    newTask.appendChild(newEditButton);
-
-    const newDeleteButton = document.createElement("button");
-    newDeleteButton.setAttribute("class", "button");
-    newDeleteButton.textContent = "Usuń zadanie";
-    newTask.appendChild(newDeleteButton, newTask);
+    createButton("button", "Edytuj", newTask, handleEditTask);
+    createButton("button", "Usuń zadanie", newTask, removeTask);
 
     taskInput.value = "";
-
-    newEditButton.addEventListener("click", editTask);
-    newDeleteButton.addEventListener("click", removeTask);
   } else {
     alert("Nazwa zadania nie może być pusta");
   }
 }
 
-function editTask(event) {
+function handleEditTask(event) {
   const button = event.target;
   const taskItem = button.parentNode;
-  const currentText = taskItem.firstChild.textContent;
-  console.log("current text", currentText);
-  const newInput = document.createElement("input");
-  newInput.value = currentText;
-  taskItem.firstChild.textContent = "";
-  taskItem.insertBefore(newInput, button);
-  button.textContent = "Zapisz zmiany";
-  button.removeEventListener("click", editTask);
-  button.addEventListener("click", saveTask);
-}
-
-function saveTask(event) {
-  const button = event.target;
-  const taskItem = button.parentNode;
-  const input = taskItem.querySelector("input");
-  if (input.value !== "") {
-    taskItem.firstChild.textContent = input.value;
-    taskItem.removeChild(input);
-    button.textContent = "Edytuj";
-    button.removeEventListener("click", saveTask);
-    button.addEventListener("click", editTask);
+  if (button.textContent === "Edytuj") {
+    const newInput = document.createElement("input");
+    const currentText = taskItem.firstChild.textContent;
+    newInput.value = currentText;
+    taskItem.firstChild.textContent = "";
+    taskItem.insertBefore(newInput, button);
+    button.textContent = "Zapisz zmiany";
   } else {
-    alert("Nazwa zadania nie może być pusta");
+    const input = taskItem.querySelector("input");
+    if (input.value !== "") {
+      taskItem.firstChild.textContent = input.value;
+      taskItem.removeChild(input);
+      button.textContent = "Edytuj";
+    } else {
+      alert("Nazwa zadanie nie może być pusta");
+    }
   }
+  // button.removeEventListener("click", handleEditTask);
+  button.addEventListener("click", handleEditTask);
 }
 
 function removeTask(event) {
-  const button = event.target;
-  const taskItem = button.parentNode;
-  const taskList = taskItem.parentNode;
-  taskList.removeChild(taskItem);
+  const taskItem = event.target.parentNode;
+  taskItem.remove();
 }
